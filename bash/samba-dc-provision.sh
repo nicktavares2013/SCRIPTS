e3c400affec49d93e250092f8f34e296a15de1d9
root <root@dc01.dominio8.lan>
2018-04-26 18:51:45 -0300

    Patch 03

--- bash/samba-dc-provision.sh
+++ bash/samba-dc-provision.sh
@@ -39,4 +39,29 @@ then
 	ln -sf /usr/local/samba/private/krb5.conf /etc/krb5.conf
 	mkdir /etc/samba
 	ln -sf /usr/local/samba/etc/smb.conf /etc/samba/smb.conf
+	cat << EOF > /usr/local/samba/private/named.conf
+dlz "AD DNS Zone" {
+      database "dlopen /usr/local/samba/lib/bind9/dlz_bind9_10.so -d 3";
+};
+EOF
 
+
+	cat << EOF >  /etc/bind/named.conf
+include "/etc/bind/named.conf.options";
+include "/etc/bind/named.conf.local";
+include "/etc/bind/named.conf.default-zones";
+include "/usr/local/samba/private/named.conf";
+EOF
+
+	cat << EOF > /etc/bind/named.conf.options
+options {
+	directory "/var/cache/bind";
+	dnssec-validation auto;
+	auth-nxdomain no;    # conform to RFC1035
+	listen-on { any; };
+	tkey-gssapi-keytab "/usr/local/samba/private/dns.keytab";
+};
+EOF
+	chmod 640 /usr/local/samba/private/dns.keytab
+	chown root:bind /usr/local/samba/private/dns.keytab
+fi
